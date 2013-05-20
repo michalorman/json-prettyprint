6d26c074a01ff7f81edfc57f03243a03c79b774a
Michal Orman <michal.orman@gmail.com>
2013-05-20 21:16:45 +0200

    Implement pretty print.

--- lib/json-prettyprint.rb
+++ lib/json-prettyprint.rb
@@ -1,7 +1,26 @@
+if RUBY_VERSION < "1.9"
+  begin
+    require 'rubygems'
+    unless Gem.available? 'json'
+      puts "\n\nPlease install 'json' gem using 'gem install json' command.\n"
+      Process.exit
+    end
+  rescue Gem::LoadError
+    puts "\n\nThe program requires either Ruby 1.9 or lower with 'rubygems' and 'json' gem installed.\n"
+  end
+end
+
+require 'json'
 require "json-prettyprint/version"
 
-module Json
-  module Prettyprint
-    # Your code goes here...
+module JSON
+  module PrettyPrint
+    def self.print input
+      json = JSON.parse input
+      pretty_json = JSON::pretty_generate json, allow_nan: true, max_nesting: false
+
+      encoder = CodeRay.encoder :term
+      encoder.encode pretty_json, :json
+    end
   end
 end
--- lib/json-prettyprint/version.rb
+++ lib/json-prettyprint/version.rb
@@ -1,5 +1,5 @@
-module Json
-  module Prettyprint
+module JSON
+  module PrettyPrint
     VERSION = "0.0.1"
   end
 end
